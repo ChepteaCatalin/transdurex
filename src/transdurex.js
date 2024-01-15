5f9e90aa333363c2d8536ea0fb9c18cc7d9591c7
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2024-01-15 08:47:32 +0200

    remove comments

--- src/transdurex.js
+++ src/transdurex.js
@@ -1,36 +1,12 @@
-/**
- * Transforms a mapping function into a reducer
- *
- * @param {Function} mapper the mapping function to convert
- * @returns {Function} reducer function
- */
-function mapReducer(mapper) {
-  return combineFn =>
-    function reducer(acc, curr) {
-      return combineFn(acc, mapper(curr));
-    };
+export function mapReducer(mapper) {
+  return combiner => (acc, curr) => combiner(acc, mapper(curr));
 }
 
-/**
- * Transforms a predicate into a reducer
- *
- * @param {Function} predicate the predicate to convert
- * @returns {Function} reducer function
- */
-function filterReducer(predicate) {
-  return combineFn =>
-    function reducer(acc, curr) {
-      return predicate(curr) ? combineFn(acc, curr) : acc;
-    };
+export function filterReducer(predicate) {
+  return combiner => (acc, curr) => predicate(curr) ? combiner(acc, curr) : acc;
 }
 
-/**
- * Creates a transducer by piping multiple reducer functions
- *
- * @param {Function[]} fns the reducers functions to pipe
- * @returns {Function} transducer function
- */
-function transducer(...fns) {
+export function transducer(...fns) {
   return fns.reduce(
     (pipe, fn) =>
       (...args) =>
@@ -38,19 +14,7 @@ function transducer(...fns) {
   );
 }
 
-/**
- * Transduces an array using the provided transducer, combiner and the initial value
- *
- * @curried
- * @param {Function} transducer
- * @param {Function} combiner
- * @param {*} initialValue
- * @param {Array} arr
- * @returns {*} accumulated value
- */
-function transduce(transducer) {
+export function transduce(transducer) {
   return combiner => initialValue => arr =>
     arr.reduce(transducer(combiner), initialValue);
 }
-
-export {mapReducer, filterReducer, transducer, transduce};
