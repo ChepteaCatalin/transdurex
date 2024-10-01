000ef133784278405f3a425bd3714c17671a1710
Catalin Cheptea <catalin.cheptea@activeops.com>
2024-10-01 19:32:00 +0300

    use arrow function expressions instead of function declarations

--- src/transdurex.js
+++ src/transdurex.js
@@ -1,20 +1,15 @@
-export function mapReducer(mapper) {
-  return combiner => (acc, curr) => combiner(acc, mapper(curr));
-}
+export const mapReducer = mapper => combiner => (acc, curr) =>
+  combiner(acc, mapper(curr));
 
-export function filterReducer(predicate) {
-  return combiner => (acc, curr) => predicate(curr) ? combiner(acc, curr) : acc;
-}
+export const filterReducer = predicate => combiner => (acc, curr) =>
+  predicate(curr) ? combiner(acc, curr) : acc;
 
-export function transducer(...fns) {
-  return fns.reduce(
+export const transducer = (...fns) =>
+  fns.reduce(
     (pipe, fn) =>
       (...args) =>
         pipe(fn(...args))
   );
-}
 
-export function transduce(transducer) {
-  return combiner => initialValue => arr =>
-    arr.reduce(transducer(combiner), initialValue);
-}
+export const transduce = transducer => combiner => initialValue => arr =>
+  arr.reduce(transducer(combiner), initialValue);
